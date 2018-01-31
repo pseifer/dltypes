eab334d6ca5a0d8f6e744078eef958250228afda
Philipp Seifer <seiferphilipp@gmail.com>
2018-01-31 11:21:21 +0100

    Add single, inline test case examples to PluginTest

--- src/test/scala/PluginTest.scala
+++ src/test/scala/PluginTest.scala
@@ -4,19 +4,41 @@ import org.scalatest._
 class PluginTest extends FreeSpec {
   TestTools.LOGLVL = 2
 
-  // Run all "success" test cases.
-  TestTools.loadFolder("success").foreach { case (name, code) =>
-    name in {
-      TestTools.testCase(name, code)
+  // Inline test cases (failure).
+
+  "Compilation should fail" in {
+    intercept[CompilationError.type] {
+      TestTools.testCase("Test0",
+"""
+val a: `:RedWine` = iri"PeterMccoyChardonnay"
+"""
+      )
     }
   }
 
-  // Run all "failure" test cases.
-  TestTools.loadFolder("failure").foreach { case (name, code) =>
-    name in {
-      intercept[CompilationError.type] {
-        TestTools.testCase(name, code)
-      }
-    }
+  // Inline test cases (success).
+
+  "Compilation should succeed" in {
+    TestTools.testCase("Test0",
+"""
+val a: `:WhiteWine` = iri"PeterMccoyChardonnay"
+"""
+    )
   }
+
+  //// Run all "success" test cases.
+  //TestTools.loadFolder("success").foreach { case (name, code) =>
+  //  name in {
+  //    TestTools.testCase(name, code)
+  //  }
+  //}
+
+  //// Run all "failure" test cases.
+  //TestTools.loadFolder("failure").foreach { case (name, code) =>
+  //  name in {
+  //    intercept[CompilationError.type] {
+  //      TestTools.testCase(name, code)
+  //    }
+  //  }
+  //}
 }
