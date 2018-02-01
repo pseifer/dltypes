ad50e17d949e6a89470a24687bb13915063b3339
Philipp Seifer <seiferphilipp@gmail.com>
2018-02-01 12:48:33 +0100

    Fix bug in DL type name extraction (for parser)
    Fix selection of DL type name, which was previously
    based on string splitting. Additionally improve debug
    output and raise error when DLType is inferred
    for value definitions.

--- src/main/scala/de/uni_koblenz/dltypes/components/CheckerAnalyzerPlugin.scala
+++ src/main/scala/de/uni_koblenz/dltypes/components/CheckerAnalyzerPlugin.scala
@@ -2,11 +2,11 @@ package de.uni_koblenz.dltypes.components
 
 import scala.tools.nsc.Global
 import scala.tools.nsc.Mode
-import scala.util.{Try => UtilTry, Success => UtilSuccess, Failure => UtilFailure}
-
+import scala.util.{Failure => UtilFailure, Success => UtilSuccess, Try => UtilTry}
 import java.io.File
 
 import de.uni_koblenz.dltypes.backend._
+import de.uni_koblenz.dltypes.runtime.DLType
 
 
 class CheckerAnalyzerPlugin(global: Global) {
@@ -33,9 +33,18 @@ class CheckerAnalyzerPlugin(global: Global) {
     val fi = new File(getClass.getResource("/wine.rdf").getFile)
     val reasoner = new ReasonerHermit(fi)
 
+    // Extract the DL type name.
+    def dlTypeName(tpe: Type): String = {
+      if (tpe.kind == "NullaryMethodType")
+        tpe.resultType.typeSymbolDirect.nameString
+      else
+        tpe.typeSymbolDirect.nameString
+    }
+
     // Attempt to parse the DLEConcept representation of an DL type.
-    def parseDL(s: String): UtilTry[DLEConcept] = {
-      parser.parse(parser.dlexpr, s.split('.').last) match {
+    def parseDL(tpe: Type): UtilTry[DLEConcept] = {
+      val s = dlTypeName(tpe)
+      parser.parse(parser.dlexpr, s) match {
         case parser.Success(m, _) => UtilSuccess(m.asInstanceOf[DLEConcept])
         case parser.NoSuccess(msg, _) => UtilFailure(new Exception(msg))
       }
@@ -53,12 +62,19 @@ class CheckerAnalyzerPlugin(global: Global) {
     def debugError(tpe: Type, pt: Type, tree: Tree, mode: Mode): Unit = {
       if (debug) {
         reporter.echo(redOn + "===[  DEBUG  ]===")
-        reporter.echo("Inferred type (tpe): " + tpe + " : " + tpe.typeSymbol.toString)
-        reporter.echo("Expected type (pt): " + pt + " : " + pt.typeSymbol.toString)
-        reporter.echo("Typed by " + typer.toString + " in mode " + mode.toString)
-        reporter.echo("---------\n" + tree.toString + "\n---------\n" + colorOff)
+        reporter.echo("Inferred type (tpe): " + tpe + " : " + tpe.typeSymbol)
+        reporter.echo("Expected type (pt): " + pt + " : " + pt.typeSymbol)
+        reporter.echo("Typed by " + typer + " in mode " + mode)
+        reporter.echo("---------\n" + tree + "\n---------\n" + colorOff)
         reporter.flush()
       }
+    }
+
+    // Print subsumed relationship in a very fancy manner.
+    def formatSubsumed(dle: DLE): String = dle match {
+      case Subsumed(l, r) =>
+        l.toString + " ⊏ " + r.toString
+      case _ => ""
     }
 
     // Report what is checked by the reasoner.
@@ -66,7 +82,7 @@ class CheckerAnalyzerPlugin(global: Global) {
       if (printchecks) {
         reporter.echo(greenOn
           + "===[ CHECKED ]===  "
-          + dle.toString
+          + formatSubsumed(dle)
           + colorOff)
         reporter.flush()
       }
@@ -75,21 +91,20 @@ class CheckerAnalyzerPlugin(global: Global) {
     // Perform type check (...).
     def typeCheck(tpe: Type, pt: Type, tree: Tree, mode: Mode): Unit = {
       if (isDLType(tpe) && isDLType(pt)) {
-        parseDL(tpe.toString).flatMap { dltpe =>
-          parseDL(pt.toString).map { dlpt =>
+        parseDL(tpe).flatMap { dltpe =>
+          parseDL(pt).map { dlpt =>
             val test = Subsumed(dltpe, dlpt)
             reportCheck(test)
             (reasoner.prove(test), test)
           }
         } match {
           case UtilFailure(msg) =>
-            reporter.error(tree.pos, "[DL] Type Parse Error: < "
-              + msg + " >")
+            reporter.error(tree.pos, "[DL] Type Parse Error: <" + msg + ">")
           case UtilSuccess((b, test)) =>
             if (!b) {
               debugError(tpe, pt, tree, mode)
-              reporter.error(tree.pos, "[DL] Type Error: < "
-                + test.toString + " > was not true.")
+              reporter.error(tree.pos, "[DL] Type Error: <"
+                + formatSubsumed(test) + "> was not true.")
             }
         }
       }
@@ -114,8 +129,16 @@ class CheckerAnalyzerPlugin(global: Global) {
       override def isActive(): Boolean = global.phase.id <= global.currentRun.typerPhase.id
 
       override def pluginsTyped(tpe: Type, typer: Typer, tree: Tree, mode: Mode, pt: Type): Type = {
+
         val tpeArgs = getTypeArgs(tpe)
         val ptArgs = getTypeArgs(pt)
+
+        // TODO: Fix this! Temp: Cases were DLType is inferred need to be annotated explicitly.
+        if (tpeArgs.map { t =>
+            mode == Mode.TYPEmode && t == typeOf[DLType] && t.toString.split('.').last == "DLType"
+          }.exists(identity))
+          reporter.error(tree.pos, "[DL] Explicit use or inference of 'DLType' violates type safety."
+            + "\nPossible solution: Declare more specific type (or top) explicitly.")
 
         if (tpeArgs.isEmpty && ptArgs.isEmpty)
           typeCheck(tpe, pt, tree, mode)
@@ -129,7 +152,6 @@ class CheckerAnalyzerPlugin(global: Global) {
         tpe
       }
     }
-
     addAnalyzerPlugin(ConcreteAnalyzerPlugin)
   }
 }
