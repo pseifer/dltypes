409aebabece19233d93354d431eef7201b876ee5
Philipp Seifer <seiferphilipp@gmail.com>
2018-02-12 18:04:04 +0100

    Rewrite isInstanceOf and type pattern cases to IRI runtime checks

--- src/main/scala/de/uni_koblenz/dltypes/components/Collector.scala
+++ src/main/scala/de/uni_koblenz/dltypes/components/Collector.scala
@@ -1,19 +1,17 @@
 package de.uni_koblenz.dltypes
 package components
 
+import scala.collection.mutable
 import scala.tools.nsc.ast.TreeDSL
-import scala.collection.mutable.{Set => MutSet}
-
 import scala.tools.nsc.Global
 import scala.tools.nsc.plugins.PluginComponent
 import scala.tools.nsc.transform.Transform
 import scala.tools.nsc.transform.TypingTransformers
 
 
-// Global symbol table that is shared between the
-// collector and Typedef phases.
 object MyGlobal {
-  val symbolTable: MutSet[String] = MutSet()
+  val symbolTable: mutable.Set[String] = mutable.Set()
+  var ontologies: mutable.MutableList[String] = mutable.MutableList()
 }
 
 
@@ -31,23 +29,88 @@ class Collector(val global: Global)
   class MyTransformer(unit: CompilationUnit) extends TypingTransformer(unit) with Extractor {
     val global: Collector.this.global.type = Collector.this.global
 
+    /*Rewrite:
+    x match {
+      case y: `:RedWine` if <guard> => <body0 with y>
+      case y: `:RedWine` => <body1 with y>
+      case _: `:RedWine` => <body2>
+      case y: Int if <guard> => <body3 with y>
+      case y: Int => <body4 with y>
+      case _: Int => <body5>
+      case _ => <body6>
+    }
+    To:
+    x match {
+      case y: IRI if y.isSubsumed(":RedWine") && <guard> =>
+        lazy val y: `:RedWine` = y.asInstanceOf[`:RedWine`]
+        <body0 with y>
+      case y: IRI if y.isSubsumed(":RedWine") =>
+        lazy val y: `:RedWine` = y.asInstanceOf[`:RedWine`]
+        <body0 with y>
+      case $fresh: IRI if $fresh.isSubsumed(":RedWine") =>
+        <body2>
+      case y: Int if <guard> => <body3 with y>
+      case y: Int => <body4 with y>
+      case _: Int => <body5>
+      case _ => <body6>
+    }*/
+    def caseToIf(c: CaseDef): Tree = {
+      c match {
+        // case x: `:RedWine` [if <guard>]* => <body>
+        // (note that desugared ==)
+        // case x @ (_: `:RedWine`) [if <guard>]* = <body>
+        case CaseDef(Bind(name, Typed(Ident(termNames.WILDCARD), DLType(tpt))), guard, body) =>
+          if (guard.isEmpty)
+            cq"$name: IRI if ${name.toTermName}.isSubsumed($tpt) => $body"
+          else
+            cq"$name: IRI if ${name.toTermName}.isSubsumed($tpt) && ($guard) => $body"
+
+        // case _: `:RedWine` [if <guard>]* => <body>
+        // (note that desugared ==)
+        // case (_: `:RedWine`) [if <guard>]* = <body>
+        case CaseDef(Typed(Ident(termNames.WILDCARD), DLType(tpt)), guard, body) =>
+          val name = newTermName("ThisIsNotAFreshName") // TODO Fix
+          if (guard.isEmpty)
+            cq"$name: IRI if $name.isSubsumed($tpt) => $body"
+          else
+            cq"$name: IRI if $name.isSubsumed($tpt) && ($guard) => $body"
+
+        // Otherwise, we don't care.
+        case _ => c
+      }
+    }
+
     override def transform(tree: Tree): Tree = tree match {
-      // Looking for ordinary DL types.
-      case DLTypeSimple(n) =>
+      // Match ordinary DL types and add them to the global symbol table for Typedef phase.
+      case DLType(n) =>
         MyGlobal.symbolTable += n
         tree
-      // Looking for the application of StringContext(<iri>).iri to List().
-      case orig @ Apply(Select(Apply(obj, List(i)), m), List())
+      case Match(l, cases) =>
+        val newCs = cases.map(caseToIf)
+        super.transform(tree)
+        atPos(tree.pos.makeTransparent)(
+          q"$l match { case ..$newCs }"
+        )
+
+      // Match the application of isInstanceOf[<DLType>] and rewrite it to isSubsumed("<DLType>").
+      case TypeApply(Select(q, name), List(DLType(tpt)))
+        if name.toString == "isInstanceOf" =>
+        val name = newTermName("ThisIsNotAFreshName") // TODO Fix
+        atPos(tree.pos.makeTransparent)(
+          q"{ val $name = $q; $name.isInstanceOf[IRI] && $name.isSubsumed($tpt) }"
+        )
+
+      // Match the application of StringContect(<iri>).iri to Nil (i.e., IRI"" literals)
+      // and add asInstanceOf to declare the nominal type.
+      case orig @ Apply(Select(Apply(obj, List(i)), m), Nil)
         if m.toString == "iri" && obj.toString == "StringContext" =>
-        // Deduce type from iri.
-        // TODO
         val newType: String = "{:" + i.toString.filter( _ != '"' ) + "}"
         MyGlobal.symbolTable += newType
         atPos(tree.pos.makeTransparent)(
           q"$orig.asInstanceOf[${newTypeName(newType)}]"
         )
-      case _ =>
-        super.transform(tree)
+
+      case _ => super.transform(tree)
     }
   }
 }
